dd5eb310c17c744d28c4dfaab2a898727236018a
claud <180181@virtualwindow.co.za>
2021-05-06 18:32:04 +0200

    remove toolbar from chat page

--- app/src/main/java/com/example/sprecan/ChatPage.kt
+++ app/src/main/java/com/example/sprecan/ChatPage.kt
@@ -1,4 +1,20 @@
 package com.example.sprecan
 
-class ChatPage {
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.shape.CornerFamily
+
+
+class ChatPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_page)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+
+    }
 }
