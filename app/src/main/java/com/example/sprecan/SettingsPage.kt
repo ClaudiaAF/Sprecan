743c59826880d4df29e3b81903c2c7fefff2c75d
claud <180181@virtualwindow.co.za>
2021-05-06 18:37:26 +0200

    settings page created

--- app/src/main/java/com/example/sprecan/SettingsPage.kt
+++ app/src/main/java/com/example/sprecan/SettingsPage.kt
@@ -1,4 +1,15 @@
 package com.example.sprecan
 
-class SettingsPage {
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_page)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+    }
 }
