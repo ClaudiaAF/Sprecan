c04276a3a82c4365eff974a26072c85eb029096a
claud <180181@virtualwindow.co.za>
2021-05-06 20:34:21 +0200

    settings page laid out

--- app/src/main/java/com/example/sprecan/SettingsPage.kt
+++ app/src/main/java/com/example/sprecan/SettingsPage.kt
@@ -2,6 +2,8 @@ package com.example.sprecan
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsPage : AppCompatActivity() {
@@ -10,6 +12,18 @@ class SettingsPage : AppCompatActivity() {
         setContentView(R.layout.activity_settings_page)
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-
+//
+//        // use arrayadapter and define an array
+//        val arrayAdapter: ArrayAdapter<*>
+//        val users = arrayOf(
+//                "Virat Kohli", "Rohit Sharma", "Steve Smith",
+//                "Kane Williamson", "Ross Taylor"
+//        )
+//
+//        // access the listView from xml file
+//        var mListView = findViewById<ListView>(R.id.list_view)
+//        arrayAdapter = ArrayAdapter(this,
+//                android.R.layout.simple_list_item_1, users)
+//        mListView.adapter = arrayAdapter
     }
 }
