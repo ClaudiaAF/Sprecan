8ec5b4de9e9090c1b31464d3dfb9b5af8062098f
claud <180181@virtualwindow.co.za>
2021-05-12 00:07:19 +0200

    center action bar title

--- app/src/main/java/com/example/sprecan/ChatPage.kt
+++ app/src/main/java/com/example/sprecan/ChatPage.kt
@@ -17,7 +17,7 @@ class ChatPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_page)
-
+        centerTitle();
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
@@ -37,5 +37,27 @@ class ChatPage : AppCompatActivity() {
         title = user.name
     }
 
-
+    private fun centerTitle() {
+        val textViews: ArrayList<View> = ArrayList()
+        window.decorView.findViewsWithText(textViews, title, View.FIND_VIEWS_WITH_TEXT)
+        if (textViews.size > 0) {
+            var appCompatTextView: AppCompatTextView? = null
+            if (textViews.size === 1) {
+                appCompatTextView = textViews[0] as AppCompatTextView
+            } else {
+                for (v in textViews) {
+                    if (v.parent is Toolbar) {
+                        appCompatTextView = v as AppCompatTextView
+                        break
+                    }
+                }
+            }
+            if (appCompatTextView != null) {
+                val params = appCompatTextView.layoutParams
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT
+                appCompatTextView.layoutParams = params
+                appCompatTextView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            }
+        }
+    }
 }
