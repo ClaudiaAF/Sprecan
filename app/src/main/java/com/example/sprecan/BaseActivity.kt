04c5f8e4c1aed83d72b84daf0de7b21976d45850
claud <180181@virtualwindow.co.za>
2021-05-11 12:52:48 +0200

    include snackbar in base activity

--- app/src/main/java/com/example/sprecan/BaseActivity.kt
+++ app/src/main/java/com/example/sprecan/BaseActivity.kt
@@ -1,7 +1,29 @@
 package com.example.sprecan
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 
 open class BaseActivity : AppCompatActivity() {
+   fun showErrorSnackBar(message: String, errorMessage: Boolean){
+        val snackBar = Snackbar.make(findViewById(R.id.content), message, Snackbar.LENGTH_LONG)
+
+        val snackBarView = snackBar.view
+
+        if(errorMessage){
+            snackBarView.setBackgroundColor(
+                    ContextCompat.getColor(
+                            this, R.color.design_default_color_error
+                    )
+            )
+        } else {
+            snackBarView.setBackgroundColor(
+                    ContextCompat.getColor(
+                            this, R.color.design_default_color_secondary
+                    )
+            )
+        }
+        snackBar.show()
+    }
 
 }
