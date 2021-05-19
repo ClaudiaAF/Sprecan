03a294c249f7fe27bcd3982786b25e5a0ae9750e
claud <180181@virtualwindow.co.za>
2021-05-19 23:58:13 +0200

    Add bio val in User class

--- app/src/main/java/com/example/sprecan/model/User.kt
+++ app/src/main/java/com/example/sprecan/model/User.kt
@@ -3,5 +3,6 @@ package com.example.sprecan.model
 class User (
         val id: String = "",
         val name: String = "",
-        val email: String = ""
+        val email: String = "",
+        val bio: String = ""
 )
