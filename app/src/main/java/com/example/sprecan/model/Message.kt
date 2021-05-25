0a3621d0a0b916a7edf8d3bcad62c4119b7237d5
claud <180181@virtualwindow.co.za>
2021-05-25 11:55:19 +0200

    constants added and message data class updated

--- app/src/main/java/com/example/sprecan/model/Message.kt
+++ app/src/main/java/com/example/sprecan/model/Message.kt
@@ -2,16 +2,14 @@ package com.example.sprecan.model
 
 import java.util.*
 
-
-object MessageType {
-    const val TEXT = "TEXT"
-    const val IMAGE = "IMAGE"
-}
-
-interface Message {
-    val time: Date
-    val senderId: String
-    val recipientId: String
-    val senderName: String
-    val type: String
+data class Message(val chatId: String,
+                   val senderId: String,
+                   val message: String,
+                   val sentDate: Date) {
+    constructor(): this(
+        "",
+        "",
+        "",
+        Date()
+    )
 }
--- app/src/main/java/com/example/sprecan/utils/Constants.kt
+++ app/src/main/java/com/example/sprecan/utils/Constants.kt
@@ -4,7 +4,11 @@ object Constants {
 
     //name all my collection labels
     const val USERS: String = "users"
-
+    const val USER_CHATS: String = "userChats"
+    const val CHAT_ID: String = "chatId"
+    const val USER_NAME: String = "userName"
+    const val CONTACT_ID: String = "contactId"
+    const val MESSAGES: String = "messages"
     //sharedpreferences
     const val LOGGED_IN_ID: String = "logged_in_id"
 }
